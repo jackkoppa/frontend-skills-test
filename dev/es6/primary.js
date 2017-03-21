ae54bc8c8e084404f00cb92375deeead190f20f3
Jack Koppa <Jack Koppa>
2017-03-21 16:26:26 -0500

    add .slide-plate styles for slide headlines

--- dev/es6/primary.js
+++ dev/es6/primary.js
@@ -2,7 +2,22 @@
 Primary scripts for populating, interacting with, and updating homepage
 */
 
+// Set carousel height
+function adjCarouselHeight() {
+	var $carousel = $(".carousel");
+	var $activeSlide = $(".carousel .slide.active") ? $(".carousel .slide.active") : $(".carousel .slide");
+	var carouselHeight = $carousel.height();
+	var slideHeight = $activeSlide.height();
+	if (carouselHeight != slideHeight) {
+		$carousel.height(slideHeight);
+	}
+}
+
+
+
 $(document).ready(function(){
 	// Handle nav menu hovers
-	
+
+	// set initial height on .carousel
+	adjCarouselHeight();
 });
